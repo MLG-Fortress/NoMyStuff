a93dfb5132ce71b857e935108174caa5d477606c
RoboMWM <robomwm@users.noreply.github.com>
2018-10-29 01:05:12 -0700

    hmm

--- src/main/java/to/us/mlgfort/NoMyStuff/NoMyStuff.java
+++ src/main/java/to/us/mlgfort/NoMyStuff/NoMyStuff.java
@@ -74,7 +74,9 @@ public class NoMyStuff extends JavaPlugin implements Listener
         {
             final Class<? extends TNTPrimed> tntClass = tnt.getClass();
             final Method getHandle = tntClass.getMethod("getHandle");
-            final Object handle = getHandle.invoke(tntClass.newInstance());
+            for (Method thing : tntClass.getMethods())
+                getLogger().info(thing.getName());
+            final Object handle = getHandle.invoke(tntClass);
             Field f = handle.getClass().getDeclaredField("source");
             Field modifiersField = Field.class.getDeclaredField("modifiers");
             modifiersField.setAccessible(true);
